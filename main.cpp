09794e821e0f1e8320afcce768c8643ef3d4ad40
Jual6332 <jual6332@colorado.edu>
2023-05-01 18:29:22 +0000

    Added command line helper client for user feedback

--- main.cpp
+++ main.cpp
@@ -31,7 +31,19 @@ class Event {
     }
 };
 
-int main() {
+int main(int argc, char** argv) {
+  cout << "Welcome to the command-line event monitoring system for the Network Monitoring team at Ford!" << endl;
+  cout << "Here are the current features:" << endl;
+  cout << "1. Ingest event data" << endl;
+  cout << "2. Display all event data" << endl;
+  cout << "3. Display router event data" << endl;
+  cout << "4. Display CPU event data" << endl;
+  cout << "Q. Quit program" << endl;
+  cout << endl;
+  cout << "What do you want to do?" << endl;
+  string answer;
+  cin >> answer;
+  
   // Define number of events
   int numEvents = 0;
   // Define number of router events
@@ -45,40 +57,44 @@ int main() {
   // Define the array collection of CPU events
   Event CPUEvents[totalPossibleEvents];
 
-  // Incoming Event Data
-  Event event1;
-  event1.setTitle("AEC router 1A is down");
-  event1.setDescription("Router went down on the weekend due to power outage.");
-  event1.setSeverity("critical");
-  allEvents[numEvents] = event1;
-  routerEvents[numRouterEvents] = event1;
-  numEvents++;
-  numRouterEvents++;
-
-  Event event2;
-  event2.setTitle("AEC router 2A is down");
-  event2.setDescription("Router went down on the weekend due to power outage.");
-  event2.setSeverity("critical");
-  allEvents[numEvents] = event2;
-  routerEvents[numRouterEvents] = event2;
-  numEvents++;
-  numRouterEvents++;
-
-  Event event3;
-  event3.setTitle("WHQ CPU27 is down");
-  event3.setDescription("CPU disconnected from server.");
-  event3.setSeverity("medium");
-  allEvents[numEvents] = event3;
-  CPUEvents[numRouterEvents] = event3;
-  numEvents++;
-  numCPUEvents++;
-
-  for (int i=0; i<numEvents; i++){
-    cout << "Event name: "<< allEvents[i].getTitle() << endl;
-    cout << "Event description: "<< allEvents[i].getDescription() << endl;
-    cout << "Event severity: "<< allEvents[i].getSeverity() << endl;
-    cout << endl;
+  if (answer == "1"){  
+    // Incoming Event Data
+    Event event1;
+    event1.setTitle("AEC router 1A is down");
+    event1.setDescription("Router went down on the weekend due to power outage.");
+    event1.setSeverity("critical");
+    allEvents[numEvents] = event1;
+    routerEvents[numRouterEvents] = event1;
+    numEvents++;
+    numRouterEvents++;
+  
+    Event event2;
+    event2.setTitle("AEC router 2A is down");
+    event2.setDescription("Router went down on the weekend due to power outage.");
+    event2.setSeverity("critical");
+    allEvents[numEvents] = event2;
+    routerEvents[numRouterEvents] = event2;
+    numEvents++;
+    numRouterEvents++;
+  
+    Event event3;
+    event3.setTitle("WHQ CPU27 is down");
+    event3.setDescription("CPU disconnected from server.");
+    event3.setSeverity("medium");
+    allEvents[numEvents] = event3;
+    CPUEvents[numRouterEvents] = event3;
+    numEvents++;
+    numCPUEvents++;
+    
+    cout << "Data successfully ingested." << endl;
+  } else if (answer == "2"){
+    // DIsplay all event data
+    for (int i=0; i<numEvents; i++){
+      cout << "Event name: "<< allEvents[i].getTitle() << endl;
+      cout << "Event description: "<< allEvents[i].getDescription() << endl;
+      cout << "Event severity: "<< allEvents[i].getSeverity() << endl;
+      cout << endl;
+    }
   }
   
-  //cout << "Hello World!\n";
 }
